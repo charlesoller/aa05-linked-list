3a1b14e298f10086dde10120e99873f2e8eb2fd4
Maazin Sherif <maazinsherif99@gmail.com>
2023-10-23 19:43:19 -0400

    Phase 2 addToHead halfway

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -8,15 +8,36 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here 
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here 
+    const newNode = new DoublyLinkedListNode(val);
+    if (this.tail === null) {
+      this.tail = newNode;
+    }
+    newNode.next = this.head; // The previous Head reassigned to the next.
+
+    // if (this.head.prev === null) {
+    //   this.head.prev = newNode;
+    //   console.log("Flag 1: ", this.head.prev)
+    // }
+    // console.log("Flag 2: ", this.head)
+    // console.log("Flag 3: ", this.head);
+    newNode.prev = null;
+    this.head = newNode;
+
+    this.length++;
+    // console.log("Flag 4: ", newNode)
+    console.log(this)
+
   }
 
+
   addToTail(val) {
-    // Your code here 
+    // Your code here
   }
 
   // You can use this function to help debug
